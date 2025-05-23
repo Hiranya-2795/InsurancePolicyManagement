28ae3e7194456e458c06285f6e74dbbab01df4ef
Hiranya-2795 <vhiranya2005@gmail.com>
2025-05-24 02:06:42 +0530

    Remove frontend folder from main branch

--- frontend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
-bootstrapApplication(AppComponent, appConfig) 
-  .catch((err) => console.error(err));
